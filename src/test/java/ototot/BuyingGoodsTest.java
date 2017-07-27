63e61ede9763e32b2813946346a0747f5f890f1c
A-Lexus <a.sokhnenko@gmail.com>
2017-07-27 06:28:24 +0300

    bla bla

--- src/test/java/ototot/BuyingGoodsTest.java
+++ src/test/java/ototot/BuyingGoodsTest.java
@@ -10,9 +10,11 @@ import ototot.Utils.DriverWraper;
 
 import java.util.concurrent.TimeUnit;
 
+import static org.testng.Assert.assertTrue;
+
 
 public class BuyingGoodsTest {
-    WebDriver driver;
+    static WebDriver driver;
 
     @BeforeTest
     public void preCondition() {
@@ -25,7 +27,7 @@ public class BuyingGoodsTest {
     }
 
 
-    @AfterTest
+    //@AfterTest
     public void postCondition() {
         driver.quit();
     }
@@ -45,24 +47,46 @@ public class BuyingGoodsTest {
         driver.findElement(By.cssSelector("input#ORDER_PROP_7")).sendKeys(PHONE_NUM);
         driver.findElement(By.cssSelector("input#ORDER_PROP_8")).sendKeys(E_MAIL);
 
-        // radio button delivery terms
+        
 
-        WebElement deliveryInTime = driver.findElement(By.cssSelector("input#ID_DELIVERY_ID_33"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute('checked','')", deliveryInTime);
+        WebElement DELIVERY_CENTR27 = driver.findElement(By.cssSelector("input#ID_DELIVERY_ID_6"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute('checked','')", DELIVERY_CENTR27);
         // radio button payment terms
         final WebElement PAYMENT_WITH_VISA_SITE = driver.findElement(By.cssSelector("input#ID_PAY_SYSTEM_ID_9"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute('checked','')", PAYMENT_WITH_VISA_SITE);
-        //driver.findElement(By.cssSelector())
+        ((JavascriptExecutor) driver).executeScript("arguments[0].setAttribute('checked','')",
+                PAYMENT_WITH_VISA_SITE);
+        final WebElement STREET_NAME = driver.findElement(By.cssSelector("div.row #ORDER_PROP_12"));
+        STREET_NAME.sendKeys("Tester");
+        final WebElement BUILD_NAME = driver.findElement(By.cssSelector("div.row #ORDER_PROP_13"));
+        BUILD_NAME.sendKeys(getBuildNum());
+        final WebElement BTN_ORDER = driver.findElement(By.cssSelector("a.btn.btn-primary.onCheckout"));
+        BTN_ORDER.click();
+        final String THANK_TEXT = driver.findElement(By.cssSelector(".thank-text")).getText();
+
+
+        assertTrue(THANK_TEXT.equalsIgnoreCase("Дякуємо за замовлення!"));
+        System.out.println("№ замовлення: " + getOrdrNum());
 
 
     }
 
-    protected static final String NAME = "Name";
+    protected static final String NAME = "TestOrder";
     protected static final String E_MAIL = "test@test.com";
     protected static final String PHONE_NUM = getPhoneNum();
 
     public static String getPhoneNum() {
 
         return String.valueOf(Math.round(Math.random() * 1000000000));
+
+    }
+
+    public static String getBuildNum() {
+
+        return String.valueOf(Math.round(Math.random() * 100));
+    }
+
+    public static String getOrdrNum() {
+
+        return driver.findElement(By.cssSelector(".col-sm-6.text-dark-grey-bold.nc")).getText();
     }
 }
